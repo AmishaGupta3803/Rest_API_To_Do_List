4e0d67f590026f381fdadf54e568f541205a8aba
AmishaGupta3803 <139355230+AmishaGupta3803@users.noreply.github.com>
2023-09-12 03:46:37 +0530

    Update urls.py

--- To_Do_List/urls.py
+++ To_Do_List/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("post-task/", post_task, name="post_task"),
     path("all-tasks/", all_tasks, name="all_tasks"),
     path("update-task/", update_task, name="update_task"),
-    path("delete-task/", delete_task, name="delete_task")
+    path("delete-task/", delete_task, name="delete_task"),
+    path("get-task/", get_task, name="get_task")
 ]
